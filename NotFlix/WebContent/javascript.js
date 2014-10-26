f754ed06182adc82ad425ec84032f8bd87162449
Marco Jansen <jansen.marco95@gmail.com>
2014-10-26 20:13:22 +0100

    aanpassingen

--- NotFlix/WebContent/javascript.js
+++ NotFlix/WebContent/javascript.js
@@ -1,6 +1,12 @@
 var token;
+var movies;
+var movie;
 
 $(document).on('pageinit','#moviespage',function(){
+    $(document).on('click','#submitrating', function() {
+    	console.log(movie.title + " " + movie.imdb + " " + $('#rating').val());
+    });
+    
     $.ajax({
         type: 'GET',
         url: 'http://localhost:8080/NotFlix/resources/movies',
@@ -8,14 +14,16 @@ $(document).on('pageinit','#moviespage',function(){
     }).fail(function(jqXHR, textStatus) {
         alert("Get movies Request failed: " + textStatus);
     }).done(function(data) {
+    	movies = data;
         $.each(data, function(index, value) {
-            $("#movielist").append("<li> <a href=#>" + value.title + "</a></li>");
+            $("#movielist").append("<li> <a href=\"#\">" + value.title + "</a></li>");
         });
         $("#movielist").listview("refresh");
     });
 });
 
 $(document).on('pageinit', '#loginpage', function(){ 
+
     $(document).on('click', '#login', function() { // catch the form's submit event
         if($('#nickname').val().length > 0 && $('#password').val().length > 0){
             // Send data to server through the ajax call
@@ -52,5 +60,18 @@ $(document).on('pageinit', '#loginpage', function(){
 });
 
 $(document).on("click", "#movielist li" ,function (event) {
-    alert("Clicked: " + $(this).text());
+	var title = jQuery.trim($(this).text());
+    $.each(movies, function(index,value) {
+    	if (title === value.title) {
+    		console.log(value);
+    		movie = value;
+    	}
+    });
+    $('#lnkDialog').click();
+    $('#movietitle').text(movie.title);
+    $('#rating').val(movie.averageRating);
+    if (movie.averageRating == 0) {
+    	$('#delete').closest('.ui-btn').hide();
+    }
 }); 
+
