b27e6e71768190b8bdc3313a47897107e1682de2
Marco Jansen <jansen.marco95@gmail.com>
2014-10-28 20:01:52 +0100

    a

--- NotFlix/WebContent/javascript/js_groot.js
+++ NotFlix/WebContent/javascript/js_groot.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-	
+	if (localStorage.getItem('nickname') !== null) {
+		$("#loginform").hide().css("visibility", "hidden");
+		$("#shownickname").empty().append("<p>Welkom " + localStorage.getItem('nickname') + "</p>").show().css("visibility", "visible");
+	}
     $.ajax({
         type: 'GET',
         url: 'http://localhost:8080/NotFlix/resources/movies',
@@ -15,6 +18,15 @@ $(document).ready(function() {
 });
 
 
+function logOut() {
+	localStorage.clear();
+	$("#loginform").show().css("visibility", "visible");
+	$("#shownickname").hide().css("visibility", "hidden");
+}
+
+/**
+ * Ajax calls from here --------------
+ */
 function logIn() {
     var nickname = $("#nickname").val();
     localStorage.setItem("nickname", nickname);
@@ -31,8 +43,45 @@ function logIn() {
         $("#shownickname").empty().append("<p>Welkom " + nickname + "</p>").show().css("visibility", "visible");
 
         $.each(data, function(index, value) {
-            localStorage.setItem("token", value);
+            localStorage.setItem("Token", value);
         });
 
     });
 }
+
+function register() {
+	$.ajax({
+		type: 'post',
+		url: 'http://localhost:8080/NotFlix/resources/users',
+		dataType: 'json',
+		//data: ,Register form .serialize(); Firstname, Lastname, Nickname, Password, (insert)
+		success: function(data) {
+			//data contains token for registered account.
+		},
+		error: function(request, error) {
+			alert("Register gone wrong!");
+		}
+			
+	});
+}
+
+function getUsers() {
+	$.ajax({
+		type: 'get',
+		url: 'http://localhost:8080/NotFlix/resources/users',
+		dataType: 'json',
+		headers: {'Token':localStorage.getItem("Token")},
+		success: function(data) {
+			$.each(data, function(index,value) {
+				// Do something with user objects ( value  equals user object)
+			});
+		}
+		error: function(request,error) {
+			alert("Get users gone wrong");
+		}
+		
+	});
+}
+
+
+
